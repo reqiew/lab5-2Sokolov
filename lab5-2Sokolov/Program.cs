8f2a9d768e5473ef118fd0f4ee4e0050e11bb142
dobry <dobry@DESKTOP-FBD3PAQ>
2023-11-29 01:31:11 +0300

    Добавьте файлы проекта.

---
+++ lab5-2Sokolov/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//using System.Text;
+//Console.WriteLine("Введите текст");
+//string sb1 = Console.ReadLine();
+//StringBuilder sb = new StringBuilder();
+//sb.Append(sb1);
+//средний уровень вариант 6 
+
+
+using System;
+using System.Text;
+  
+        string inputString = Console.ReadLine();
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in inputString)
+        {
+            result.Append(c);
+            result.Replace("*","<>"); //я не понял что требовалось вставить после * поэтому поставил <>
+        }
+
+        string outputString = result.ToString();
+        Console.WriteLine(outputString);
